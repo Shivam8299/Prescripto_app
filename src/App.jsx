6dd45252e9bad3452a348e6c3a67fa9a1ef9d10f
Shivam8299 <shivammishra03812gmail.com>
2024-12-17 23:49:04 +0530

    save commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Contact from './pages/Contact'
 import Appointment from './pages/Appointment'
 
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
         <Route path='/my-profile' element={<MyProfile />}/>
         <Route path='/appointment/:docId' element={<Appointment />}/>
       </Routes>
+      <Footer/>
       
     </div>
   )
