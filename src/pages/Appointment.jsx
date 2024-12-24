686865560778af6f0e1636263ff8ab39b752a131
Shivam8299 <shivammishra0381@gmail.com>
2024-12-24 23:16:30 +0530

    new update

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 function Appointment() {
+  const {docId} = useParams()
+  const {doctors} = useContext(AppContext)
+  const [docInfo, setDocInfo] = useState(null)
+
+  const fetchDocInfo = async ()=> {
+    const foundDoc = doctors.find(doc => doc._id === docId)
+      setDocInfo(foundDoc)
+      console.log(foundDoc)
+  }
+
+  useEffect(()=>{
+    fetchDocInfo() 
+  },[doctors,docId])
   return (
     <div>
-      
+      {/* doctor's details  */}
+      <div>
+        <div>
+          {docInfo.name}
+        </div>
+      </div>
     </div>
   )
 }
